6f75c4c5f29db316654634cb50eede17d1fd518f
bainanaresh <bainanaresh407@gmail.com>
2024-09-15 17:19:59 +0530

    changes added here

--- src/test/java/com/baina/JenkinsCiCdDemoApplicationTests.java
+++ src/test/java/com/baina/JenkinsCiCdDemoApplicationTests.java
@@ -1,10 +1,10 @@
 package com.baina;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,19 +16,18 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class JenkinsCiCdDemoApplicationTests {
 
 	private MockMvc mockMvc;
 	@Autowired
 	private WebApplicationContext context;
 	
-	//@Test
+	@Test
 	void contextLoads() throws Exception {
 		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-		MvcResult result = mockMvc.perform(get("/naresh").contentType(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/naresh").contentType(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 		String resultContent = result.getResponse().getContentAsString();
 		Assert.assertEquals(resultContent , "welcome to web app naresh");
 	}
